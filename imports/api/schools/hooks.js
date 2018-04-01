a74dd13a02e01266775950274743a2ba770857f0
Pierre Ferreira <pierre@tektite.biz>
2018-04-02 01:27:36 +0200

    School create from with uniforms (hooks added) #2.

--- imports/api/schools/hooks.js
+++ imports/api/schools/hooks.js
@@ -1,13 +1,11 @@
-// import Schools from './collection';
-//
-// console.log('Schools:',Schools)
-//
-// Schools.before.insert((userId, doc) => {
-//   doc.userId = userId;
-//   doc.createdAt = Date.now();
-//   doc.updatedAt = Date.now();
-// });
-//
-// Schools.before.update((userId, doc) => {
-//   doc.updatedAt = Date.now();
-// });
+import Schools from './collection';
+
+Schools.before.insert((userId, doc) => {
+  doc.userId = userId;
+  doc.createdAt = Date.now();
+  doc.updatedAt = Date.now();
+});
+
+Schools.before.update((userId, doc) => {
+  doc.updatedAt = Date.now();
+});
